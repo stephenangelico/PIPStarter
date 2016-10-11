eede6a2587fd4accdb163133cdbbc851387191b8
Stephen Angelico <raaff35lightning@gmail.com>
2016-10-12 09:55:06 +1100

    PirateBartender: make indentation consistent

--- PirateBartender.py
+++ PirateBartender.py
@@ -2,19 +2,19 @@
 # Pirate Bartender for PIP 1.3.2
 import random
 questions = {
-    "strong": "Do ye like yer drinks strong? ",
-    "salty": "Do ye like it with a salty tang? ",
-    "bitter": "Are ye a lubber who likes it bitter? ",
-    "sweet": "Would ye like a bit of sweetness with yer poison? ",
-    "fruity": "Are ye one for a fruity finish? ",
+	"strong": "Do ye like yer drinks strong? ",
+	"salty": "Do ye like it with a salty tang? ",
+	"bitter": "Are ye a lubber who likes it bitter? ",
+	"sweet": "Would ye like a bit of sweetness with yer poison? ",
+	"fruity": "Are ye one for a fruity finish? ",
 }
 
 ingredients = {
-    "strong": ["glug of rum", "slug of whisky", "splash of gin"],
-    "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
-    "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
-    "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
-    "fruity": ["slice of orange", "dash of cassis", "cherry on top"],
+	"strong": ["glug of rum", "slug of whisky", "splash of gin"],
+	"salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
+	"bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
+	"sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
+	"fruity": ["slice of orange", "dash of cassis", "cherry on top"],
 }
 customers = {}
 preferences = {}
@@ -23,6 +23,9 @@ recipe = []
 def loyalty():
 	customername = input("Avast, landlubber! Who goes there?")
 	if customers.get(customername) == '':
+		print("Ye must be new here.")
+		order()
+	else:
 		usual = input("Arr! Welcome back! Yer usual?")
 		if usual == 'y' or usual == 'Y' or usual == 'yes' or usual == 'Yes':
 			usual = True
@@ -30,22 +33,21 @@ def loyalty():
 			usual = False
 
 def order():
-    for dict_key in questions:
-        ingredtype = input(questions[dict_key])
-        if ingredtype == 'y' or ingredtype == 'Y' or ingredtype == 'yes' or ingredtype == 'Yes':
-           ingredtype = True
-        else:
-           ingredtype = False
-        preferences[dict_key] = ingredtype
+	for dict_key in questions:
+	ingredtype = input(questions[dict_key])
+	if ingredtype == 'y' or ingredtype == 'Y' or ingredtype == 'yes' or ingredtype == 'Yes':
+		ingredtype = True
+	else:
+		ingredtype = False
+	preferences[dict_key] = ingredtype
 
 def make():
-   for preference in preferences:
-       if preferences[preference] == True:
-           recipe.append(random.choice(ingredients[preference]))
+	for preference in preferences:
+		if preferences[preference] == True:
+			recipe.append(random.choice(ingredients[preference]))
 
 if __name__ == '__main__':
 	order()
 	print(preferences)
 	make()
 	print(recipe)
-	
