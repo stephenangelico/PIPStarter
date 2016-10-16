da9aa03d92bcbe229cd0e269bd1ae8ad8d0494f8
Stephen Angelico <raaff35lightning@gmail.com>
2016-10-17 00:49:16 +1100

    PirateBartender: un-global variables to avoid leftover values

--- PirateBartender.py
+++ PirateBartender.py
@@ -17,45 +17,34 @@ ingredients = {
 	"fruity": ["slice of orange", "dash of cassis", "cherry on top"],
 }
 customers = {}
-preferences = {}
-recipe = []
 def loyalty():
 	customername = input("Avast, landlubber! Who goes there? ")
 	try:
 		customers[customername] == ''
 	except KeyError:
 		print("Ye must be new here.")
-		order()
-		make()
-		customers[customername] = recipe
+		customers[customername] = order()
 
 	else:
 		usual = input("Arr! Welcome back! Yer usual? ")
 		if usual == 'y' or usual == 'Y' or usual == 'yes' or usual == 'Yes':
 			print(customers[customername])
 		else:
-			order()
-			make()
-			customers[customername] = recipe
+			customers[customername] = order()
+	return(customers[customername])
 
 def order():
-	del recipe[:]
+	recipe = []
 	for question in questions:
 		ingredtype = input(questions[question])
 		if ingredtype == 'y' or ingredtype == 'Y' or ingredtype == 'yes' or ingredtype == 'Yes':
-			ingredtype = True
-		else:
-			ingredtype = False
-		preferences[question] = ingredtype
+			recipe.append(make(question))
+	return(recipe)
 
-def make():
-	for preference in preferences:
-		if preferences[preference] == True:
-			recipe.append(random.choice(ingredients[preference]))
+def make(preference):
+	return(random.choice(ingredients[preference]))
 
 while __name__ == '__main__':
-	loyalty()
-	print(preferences)
-	print(recipe)
-	print(customers)
-	preferences.clear()
+	print(loyalty())
+	#print(customers)
+	
