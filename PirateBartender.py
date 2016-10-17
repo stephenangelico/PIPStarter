88eca95c9f4689c929d0271df91a242682fb5634
Stephen Angelico <raaff35lightning@gmail.com>
2016-10-17 14:09:03 +1100

    PirateBartender: implement drink naming

--- PirateBartender.py
+++ PirateBartender.py
@@ -16,6 +16,28 @@ ingredients = {
 	"sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
 	"fruity": ["slice of orange", "dash of cassis", "cherry on top"],
 }
+ingredmapping = {
+	"glug of rum": 1,
+	"slug of whisky": 2,
+	"splash of gin": 4,
+	"olive on a stick": 8,
+	"salt-dusted rim": 16,
+	"rasher of bacon": 32,
+	"shake of bitters": 64,
+	"splash of tonic": 128,
+	"twist of lemon peel": 256,
+	"sugar cube": 512,
+	"spoonful of honey": 1024,
+	"spash of cola": 2048,
+	"slice of orange": 4096,
+	"dash of cassis": 8192,
+	"cherry on top": 16384,
+}
+# Known bug: not enough naming possibilities for all ingredients
+# Known bug: no way to handle random choosing collisions
+nameadjectives = ["Acidic","Barnyard","Bright","Charcoal","Chocolaty","Creamy","Elegant","Expressive","Grassy","Juicy","Lean","Musty","Oxidized","Round","Smokey","Spicy","Supple","Tart","Vanillin","Velvety"]
+namenouns = ["Apple","Blackcurrant","Blood Orange","Dried Lime","Ginger","Lychee","Mace","Mango","Mulberry","Nectarine","Nutmeg","Quince","Papaya","Passionfruit","Pineapple","Raspberry","Rose","Sarsaparilla","Watermelon","Zest"]
+drinknames = {}
 customers = {}
 def loyalty():
 	customername = input("Avast, landlubber! Who goes there? ")
@@ -28,7 +50,7 @@ def loyalty():
 	else:
 		usual = input("Arr! Welcome back! Yer usual? ")
 		if usual == 'y' or usual == 'Y' or usual == 'yes' or usual == 'Yes':
-			print(customers[customername])
+			pass
 		else:
 			customers[customername] = order()
 	return(customers[customername])
@@ -39,10 +61,20 @@ def order():
 		ingredtype = input(questions[question])
 		if ingredtype == 'y' or ingredtype == 'Y' or ingredtype == 'yes' or ingredtype == 'Yes':
 			recipe.append(make(question))
-	return(recipe)
+	return(recipe,drinkname(recipe))
 
 def make(preference):
 	return(random.choice(ingredients[preference]))
+
+def drinkname(recipe):
+	drinkid = 0
+	for ingredient in recipe:
+		drinkid += ingredmapping[ingredient]
+	try:
+		drinknames[drinkid] == ''
+	except KeyError:
+		drinknames[drinkid] = random.choice(nameadjectives) + ' ' + random.choice(namenouns)
+	return(drinknames[drinkid])
 
 while __name__ == '__main__':
 	print(loyalty())
