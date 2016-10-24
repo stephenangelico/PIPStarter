985d8f65ec8329ffa737ebbf65ff4040d58b9cff
Stephen Angelico <raaff35lightning@gmail.com>
2016-10-24 11:41:59 +1100

    class_demo: Add band class and define initial band

--- class_demo.py
+++ class_demo.py
@@ -1,5 +1,6 @@
 class Musician(object):
-	def __init__(self, sounds):
+	def __init__(self, sounds, name):
+		self.name = name
 		self.sounds = sounds
 	def solo(self, length):
 		for i in range(length):
@@ -7,20 +8,41 @@ class Musician(object):
 		print()
 
 class Bassist(Musician):
-	def __init__(self):
-		super().__init__(["Twang", "Thrumb", "Bling"])
+	def __init__(self, name):
+		super().__init__(["Twang", "Thrumb", "Bling"], name)
 
 class Guitarist(Musician):
-	def __init__(self):
-		super().__init__(["Boink", "Bow", "Boom"])
+	def __init__(self, name):
+		super().__init__(["Boink", "Bow", "Boom"], name)
 	def tune(self):
 		print("Be with you in a moment")
 		print("Twoning, sproing, splang")
 
 class Drummer(Musician):
-	def __init__(self):
-		super().__init__(["Boom", "Crash", "Ting"])
-	def count_time():
+	def __init__(self, name):
+		super().__init__(["Boom", "Crash", "Ting"], name)
+	def count_time(self):
 		print("One, Two, Three, Four!")
-	def combust():
+	def combust(self):
 		print("FOOOM!")
+
+class Band(object):
+	def __init__(self, guitarist, drummer):
+		self.members = []
+		self.guitarist = guitarist
+		self.drummer = drummer
+		self.members.append(guitarist)
+		self.members.append(drummer)
+		self.guitarist.tune()
+	def jam(self):
+		self.drummer.count_time()
+		for member in self.members:
+			member.solo(4)
+	def hire(self, musician):
+		self.members.append(musician)
+		print("Please welcome {} to the stage!".format(musician.name))
+		
+PIP_Rollers = Band(Guitarist("Matthew"), Drummer("Ben"))
+PIP_Rollers.jam()
+PIP_Rollers.hire(Bassist("Henry"))
+PIP_Rollers.jam()
