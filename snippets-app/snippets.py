f7d46586641670e4d80e6d7198b7b26508e05e08
Stephen Angelico <raaff35lightning@gmail.com>
2016-11-19 12:30:27 +1100

    snippets: Add sub-parser

--- snippets-app/snippets.py
+++ snippets-app/snippets.py
@@ -8,6 +8,15 @@ def main():
 	"""Main function"""
 	logging.info("Constructing parser")
 	parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+	
+	subparsers = parser.add_subparsers(dest="command", help="Available commands")
+	
+	# Subparser for the put command
+	logging.debug("Constructing put subparser")
+	put_parser = subparsers.add_parser("put", help="Store a snippet")
+	put_parser.add_argument("name", help="Name of the snippet")
+	put_parser.add_argument("snippet", help="Snippet text")
+	
 	arguments = parser.parse_args()
 def put(name, snippet):
 	"""
