b6206938018a76b6d63f1043062e1d03110fca98
Stephen Angelico <raaff35lightning@gmail.com>
2016-12-10 23:48:35 +1100

    Start new tbay project

---
+++ tbay/tbay.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Online auction project - main service
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
+
+engine = create_engine('postgresql://stephen:thinkful@127.0.0.1:5432/tbay')
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+class Item(Base):
+	__tablename__ = "items"
+	
+	id = Column(Integer, primary_key=True)
+	name = Column(String, nullable=False)
+	description = Column(String)
+	start_time = Column(DateTime, default=datetime.utcnow)
+
+Base.metadata.create_all(engine)
