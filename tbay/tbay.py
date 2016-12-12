0069877c4ff8308ccd588133f7d17a1a6511223e
Stephen Angelico <raaff35lightning@gmail.com>
2016-12-12 11:10:00 +1100

    tbay: Add Customer and Bid classes

--- tbay/tbay.py
+++ tbay/tbay.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, Float, String, DateTime
 
 engine = create_engine('postgresql://stephen:thinkful@localhost:5432/tbay')
 Session = sessionmaker(bind=engine)
@@ -18,5 +18,20 @@ class Item(Base):
 	name = Column(String, nullable=False)
 	description = Column(String)
 	start_time = Column(DateTime, default=datetime.utcnow)
+
+class Customer(Base):
+	__tablename__ = "customers"
+	
+	uid = Column(Integer, primary_key=True)
+	username = Column(String, nullable=False)
+	password = Column(String, nullable=False)
+	creation_time = Column(DateTime, default=datetime.utcnow)
+
+class Bid(Base):
+	__tablename__ = "bids"
+	
+	bidid = Column(Integer, primary_key=True)
+	price = Column(Float, nullable=False)
+	bid_time = Column(DateTime, default=datetime.utcnow)
 
 Base.metadata.create_all(engine)
