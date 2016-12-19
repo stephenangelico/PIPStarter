6e090716e5363f53068750ab50eac7042305fdec
Stephen Angelico <raaff35lightning@gmail.com>
2016-12-19 14:13:37 +1100

    tbaycontent: Use session.add_all instead of add

--- tbay/content_seed.py
+++ tbay/content_seed.py
@@ -7,5 +7,5 @@ stephen = User(username="stephena", password="guesswhat")
 buzz = Item(name="Buzz Lightyear Action Figure", description="12-inch Buzz Lightyear action toy with wings, lights and sounds", category="Toys")
 dresser = Item(name="Oak Standing Dresser", description="Red Oak 3-drawer standing dresser", category="Furniture")
 
-session.add(beyonce, stephen, buzz, dresser)
+session.add_all([beyonce, stephen, buzz, dresser])
 session.commit()
