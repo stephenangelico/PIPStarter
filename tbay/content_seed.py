0be7a34d5bc672b2b82848f6d541ebe882bbdc21
Stephen Angelico <raaff35lightning@gmail.com>
2017-01-10 16:26:52 +1100

    tbaycontent: Add third user and baseball

--- tbay/content_seed.py
+++ tbay/content_seed.py
@@ -3,9 +3,11 @@ from tbay import *
 
 beyonce = User(username="bknowles", password="uhohuhohuhohohunana")
 stephen = User(username="stephena", password="guesswhat")
+woody = User(username="sheriffwoody", password="theresasnakeinmyboot")
 
-buzz = Item(name="Buzz Lightyear Action Figure", description="12-inch Buzz Lightyear action toy with wings, lights and sounds", category="Toys")
-dresser = Item(name="Oak Standing Dresser", description="Red Oak 3-drawer standing dresser", category="Furniture")
+buzz = Item(name="Buzz Lightyear Action Figure", description="12-inch Buzz Lightyear action toy with wings, lights and sounds", category="Toys", user="bknowles")
+dresser = Item(name="Oak Standing Dresser", description="Red Oak 3-drawer standing dresser", category="Furniture", user="stephena")
+ball = Item(name="Baseball", description="Well-loved baseball", category="Sporting Equipment", user="sheriffwoody")
 
-session.add_all([beyonce, stephen, buzz, dresser])
+session.add_all([beyonce, stephen, woody, buzz, dresser, ball])
 session.commit()
