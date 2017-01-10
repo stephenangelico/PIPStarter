37fdaea34f5375f787764311d87e6556f748bbe0
Stephen Angelico <raaff35lightning@gmail.com>
2017-01-10 16:27:37 +1100

    tbay: Add relationships between tables

--- tbay/tbay.py
+++ tbay/tbay.py
@@ -4,12 +4,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
-from sqlalchemy import Column, Integer, Float, String, DateTime
+from sqlalchemy import Table, Column, Integer, Float, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 
 engine = create_engine('postgresql://stephen:thinkful@localhost:5432/tbay')
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
+
+user_bids_table = Table('user_bids_association', Base.metadata,
+	Column('user_id', Integer, ForeignKey('users.uid')),
+	Column('bid_id', Integer, ForeignKey('bids.bidid')),
+	Column('item_id', Integer, ForeignKey('items.id'))
+)
 
 class Item(Base):
 	__tablename__ = "items"
@@ -27,6 +34,9 @@ class User(Base):
 	username = Column(String, nullable=False)
 	password = Column(String, nullable=False)
 	creation_time = Column(DateTime, default=datetime.utcnow)
+	items_auctioned = relationship("Item", backref="user")
+	items_bidded = relationship("Item", backref="user")
+	bids = relationship("Bid", secondary="user_bids_association", backref="user")
 
 class Bid(Base):
 	__tablename__ = "bids"
@@ -34,6 +44,7 @@ class Bid(Base):
 	bidid = Column(Integer, primary_key=True)
 	price = Column(Float, nullable=False)
 	bid_time = Column(DateTime, default=datetime.utcnow)
+	item = relationship("Item", backref="bid")
 
 if __name__ == "__main__":
 	Base.metadata.drop_all(engine)
