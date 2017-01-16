88d31edb275ab82e65662664d0c816e4f83e0768
Stephen Angelico <raaff35lightning@gmail.com>
2017-01-16 14:51:44 +1100

    tbay: Fix table relationships

--- tbay/tbay.py
+++ tbay/tbay.py
@@ -12,11 +12,16 @@ Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
 
-user_bids_table = Table('user_bids_association', Base.metadata,
-	Column('user_id', Integer, ForeignKey('users.uid')),
-	Column('bid_id', Integer, ForeignKey('bids.bidid')),
-	Column('item_id', Integer, ForeignKey('items.id'))
-)
+#user_bids_table = Table('user_bids_association', Base.metadata,
+#	Column('user_id', Integer, ForeignKey('users.uid')),
+#	Column('bid_id', Integer, ForeignKey('bids.bidid')),
+#	Column('item_id', Integer, ForeignKey('items.id'))
+#)
+
+# Relationships:
+# User:Bid = One-to-Many
+# Item:Bid = One-to-Many
+# User:Item = One-to-Many
 
 class Item(Base):
 	__tablename__ = "items"
@@ -26,7 +31,9 @@ class Item(Base):
 	description = Column(String)
 	category = Column(String, nullable=False)
 	start_time = Column(DateTime, default=datetime.utcnow)
-
+	owner_uid = Column(Integer, ForeignKey('users.uid'), nullable=False)
+	bids = relationship("Bid", backref="item")
+	
 class User(Base):
 	__tablename__ = "users"
 	
@@ -34,9 +41,8 @@ class User(Base):
 	username = Column(String, nullable=False)
 	password = Column(String, nullable=False)
 	creation_time = Column(DateTime, default=datetime.utcnow)
-	items_auctioned = relationship("Item", backref="user")
-	items_bidded = relationship("Item", backref="user")
-	bids = relationship("Bid", secondary="user_bids_association", backref="user")
+	items_auctioned = relationship("Item", backref="owner")
+	bids = relationship("Bid", backref="bidder")
 
 class Bid(Base):
 	__tablename__ = "bids"
@@ -44,7 +50,21 @@ class Bid(Base):
 	bidid = Column(Integer, primary_key=True)
 	price = Column(Float, nullable=False)
 	bid_time = Column(DateTime, default=datetime.utcnow)
-	item = relationship("Item", backref="bid")
+	item_id = Column(Integer, ForeignKey('items.id'), nullable=False)
+	bidder_uid = Column(Integer, ForeignKey('users.uid'), nullable=False)
+
+def createbid(price, itemname, username):
+	#bid1 = Bid(price=20, item_id=1, bidder_uid=2)
+	#              ^ amount    ^ Buzz       ^ user stephena
+	# Returns the description of all of the basesballs
+	#session.query(Item.description).filter(Item.name == "baseball").all()
+	#A new bid must be greater than the last one
+	
+
+def endauction(item_id):
+	#Find all bids that are associated with given item ID
+	# Return the item id and description for all baseballs which were created in the past.  Remember to import the datetime object: from datetime import datetime
+	#session.query(Item.id, Item.description).filter(Item.name == "baseball", Item.start_time < datetime.utcnow()).all()
 
 if __name__ == "__main__":
 	Base.metadata.drop_all(engine)
