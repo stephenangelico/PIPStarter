18f1f868f504715f2349c98b007353216ce1fdeb
Stephen Angelico <raaff35lightning@gmail.com>
2017-01-16 14:51:59 +1100

    tbaycontent: Adjust to allow for new relationships

--- tbay/content_seed.py
+++ tbay/content_seed.py
@@ -5,9 +5,12 @@ beyonce = User(username="bknowles", password="uhohuhohuhohohunana")
 stephen = User(username="stephena", password="guesswhat")
 woody = User(username="sheriffwoody", password="theresasnakeinmyboot")
 
-buzz = Item(name="Buzz Lightyear Action Figure", description="12-inch Buzz Lightyear action toy with wings, lights and sounds", category="Toys", user="bknowles")
-dresser = Item(name="Oak Standing Dresser", description="Red Oak 3-drawer standing dresser", category="Furniture", user="stephena")
-ball = Item(name="Baseball", description="Well-loved baseball", category="Sporting Equipment", user="sheriffwoody")
+session.add_all([beyonce, stephen, woody])
+session.commit()
 
-session.add_all([beyonce, stephen, woody, buzz, dresser, ball])
+buzz = Item(name="Buzz Lightyear Action Figure", description="12-inch Buzz Lightyear action toy with wings, lights and sounds", category="Toys", owner_uid=beyonce.uid)
+dresser = Item(name="Oak Standing Dresser", description="Red Oak 3-drawer standing dresser", category="Furniture", owner_uid=stephen.uid)
+ball = Item(name="Baseball", description="Well-loved baseball", category="Sporting Equipment", owner_uid=woody.uid)
+
+session.add_all([buzz, dresser, ball])
 session.commit()
